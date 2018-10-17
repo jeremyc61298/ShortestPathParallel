680dbc03e23a187bf238676ce118f10a42d7d0e9
Jeremy Campbell <jeremyc61298@gmail.com>
2018-10-16 20:30:47 -0500

    Program will work iteratively. There are currently debug statements still present.

--- ShortestPathParallel/shortest.cpp
+++ ShortestPathParallel/shortest.cpp
@@ -9,13 +9,15 @@
 #include <omp.h>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::vector;
+using std::min;
 
-//#define DEBUG
+#define DEBUG
 
 struct RoadMap
 {
@@ -25,15 +27,15 @@ struct RoadMap
 	vector<vector<int>> routes;
 	vector<double> lengths;
 	vector<vector<double>> adjacency;
+	vector<int> shortestRoute;
 
 	void readRoads();
 	void readRoutes();
 	void computeShortestLengths();
-	void outputLengths();
+	void outputResults();
 
 private:
-	void computeShortestPaths();
-		
+	void computeShortestPaths(); 
 };
 
 void RoadMap::readRoads()
@@ -70,18 +72,63 @@ void RoadMap::readRoutes()
 
 void RoadMap::computeShortestLengths()
 {
-	
+	// Set the adjacency matrix so that it contains
+	// all the shortest paths to between any 2 locations
+	computeShortestPaths();
+	lengths.resize(routes.size());
+	double shortestLength = INFINITY;
+	int shortestRouteIndex;
 
+	for (int i = 0; i < routes.size(); i++)
+	{
+		double total = 0;
+		cout << "Numbers for route " << i << ": ";
+		for (int j = 0; j < routes[i].size() - 1; j++)
+		{
+			// Add up all the distances from each point in the route. 
+			total += adjacency[routes[i][j]][routes[i][j+1]];
+			cout << adjacency[routes[i][j]][routes[i][j + 1]] << " ";
+		}
+		lengths[i] = total;
+		cout << endl;
+		// Keep track of the shortest route
+		if (total < shortestLength)
+		{
+			shortestLength = total;
+			shortestRouteIndex = i;
+		}
+	}
+
+	shortestRoute = routes[shortestRouteIndex];
 }
 
+// Floyd-Warshall Algorithm
+// Calculates the shortest path between 
+// any 2 verticies in a graph
 void RoadMap::computeShortestPaths()
 {
-	// Floyd-Warshall Algorithm
+	const int locations = adjacency.size();
+	for (int k = 0; k < locations; k++)
+	{
+		for (int i = 0; i < locations; i++)
+		{
+			for (int j = 0; j < locations; j++)
+			{
+				adjacency[i][j] = min(adjacency[i][j], adjacency[i][k] + adjacency[k][j]);
+			}
+		}
+	}
 }
 
-void RoadMap::outputLengths()
+void RoadMap::outputResults()
 {
+	for (double length : lengths)
+		cout << length << endl;
 
+	for (int location : shortestRoute)
+		cout << location << " ";
+
+	cout << endl;
 }
 
 template <typename T>
@@ -102,18 +149,15 @@ int main()
 {
 	RoadMap roadMap;
 	roadMap.readRoads();
+	roadMap.readRoutes();
+	roadMap.computeShortestLengths();
 
+	cout << "New Adjacency Matrix: " << endl;
+	cout << "-------------------------------------" << endl;
 #ifdef DEBUG
 	print2DVector(roadMap.adjacency);
 #endif
-
-	roadMap.readRoutes();
-
-#ifdef DEBUG
-	print2DVector(roadMap.routes);
-#endif
-
-	// What next?
-	//computeShortestLengths(routes, adjacency);
-	//outputLengths();
+	cout << "-------------------------------------" << endl;
+	
+	roadMap.outputResults();
 }
